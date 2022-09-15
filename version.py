1ec64c361cd4d3c56afbc130273e3e83ac664452
hgshah <hgshah@ualberta.ca>
2022-09-14 18:25:40 -0600

    updated

--- version.py
+++ version.py
@@ -1,3 +1,5 @@
-import webbrowser
-url = 'http://google.com/'
-webbrowser.open(url)
+import requests
+print(requests.__version__)
+req = requests.get("http://google.com/")
+print(req)
+print(req.text)
